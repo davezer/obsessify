0dc2964fdab5d28ef15fdddafd9bbe76a66c22e7
Adam Calcasola <adam.calcasola@gmail.com>
2022-03-03 20:31:03 -0500

    added some login features

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const startServer = async () => {
 
 startServer();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
