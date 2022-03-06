c5482eb72911c86b9a809cacc1877fe7c0b5e237
MFG <macfglenn@gmail.com>
2022-03-05 21:20:50 -0500

    added Landing page to navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,33 +1,35 @@
 import React from "react";
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 function Navigation(props) {
-  const tabs = ['MyCollection']
+  const tabs = ["Landing", "MyCollection"];
   return (
     <Navbar className="navbar sticky-top" id="navbar" bg="" expand="md">
       <Container className="brand-con">
-        <Navbar.Brand className ="brand" href="./index.html">Obsessify</Navbar.Brand>
+        <Navbar.Brand className="brand" href="./index.html">
+          Obsessify
+        </Navbar.Brand>
       </Container>
-      <Container>  
+      <Container>
         <Nav className="nav-links">
-          {tabs.map(tab => (
-          <ul className="nav-item" key={tab}>
-            <a
-              href={'#' + tab.toLowerCase()}
-              onClick={() => props.handlePageChange(tab)}
-              className={
-                props.currentPage === tab ? 'nav-link active' : 'nav-link'
-              }
-            >
-              {tab}
-            </a>
-          </ul>
-        ))}
+          {tabs.map((tab) => (
+            <ul className="nav-item" key={tab}>
+              <a
+                href={"#" + tab.toLowerCase()}
+                onClick={() => props.handlePageChange(tab)}
+                className={
+                  props.currentPage === tab ? "nav-link active" : "nav-link"
+                }
+              >
+                {tab}
+              </a>
+            </ul>
+          ))}
           {/* <Nav.Link className="nav-link" href="#about">About</Nav.Link>
           <Nav.Link className="nav-link" href="#experience">Experience</Nav.Link>
           <Nav.Link className="nav-link" href="#featured">Projects</Nav.Link>
           <Nav.Link className="nav-link" href="#contact">Contact</Nav.Link> */}
-          </Nav>
+        </Nav>
       </Container>
     </Navbar>
   );
