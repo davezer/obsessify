2b30f668981d5ca04001ed549b1e8db6762a367c
MFG <macfglenn@gmail.com>
2022-03-05 21:44:08 -0500

    rearranged some code, need to change text color and location of links

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,16 +4,14 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 function Navigation(props) {
   const tabs = ["Landing", "MyCollection"];
   return (
-    <Navbar className="navbar sticky-top" id="navbar" bg="" expand="md">
+    <Navbar className="navbar sticky-top" id="navbar" bg="dark" expand="md">
       <Container className="brand-con">
         <Navbar.Brand className="brand" href="./index.html">
           Obsessify
         </Navbar.Brand>
-      </Container>
-      <Container>
-        <Nav className="nav-links">
+        <Nav className="nav-links me-auto">
           {tabs.map((tab) => (
-            <ul className="nav-item" key={tab}>
+            <ul className="nav-item flex" key={tab}>
               <a
                 href={"#" + tab.toLowerCase()}
                 onClick={() => props.handlePageChange(tab)}
